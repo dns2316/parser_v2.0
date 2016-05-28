3491dbebf85e07079a04737b43e97966cd82fef8
dns2316 <dns2316@gmail.com>
2016-05-28 18:18:05 +0300

    step-2

--- sand_v3.0.php
+++ sand_v3.0.php
@@ -12,44 +12,73 @@ require_once 'functions.php';
 
     $bd = mysqli_connect("localhost", "gtx", "678352saqejgf2", "php_dns");
 
-
-
-
-
+//парсим страницу
 
 $html = file_get_html('http://habrahabr.ru');
 $element='empty';
 $date_nextd=date('y-m-d h:i',strtotime('+1 day'));
 $date1=date('y-m-d h:i');
 foreach($html->find('div.post') as $p => $item){
-  $title1 = (string)implode(" ", $item->find('h2.post__title'));
-  $text1 = (string)implode(" ", $item->find('.html_format'));
-  $url_a = (string)implode(" ", $item->find('a.button'));
+  $title1 = implode(" ", $item->find('h2.post__title'));
+  $text1 = implode(" ", $item->find('.html_format'));
+  $url_a = implode(" ", $item->find('a.button'));
   $url_b = explode('"', $url_a);
   $html_in = file_get_html($url_b[3]);
-  $stringurl=(string)$url_b[3];
-  $full_text = (string)implode(" ", $html_in->find('.html_format'));
-// print_r ($title1);
-// echo $title1;
+  $stringurl=$url_b[3];
+  $full_text = implode(" ", $html_in->find('.html_format'));
+
+//Преобразование в "сущности" - убирает кавычки html
+
+  $title1 = htmlspecialchars($title1, ENT_QUOTES);
+  $text1 = htmlspecialchars($text1, ENT_QUOTES);
+  $date1 = htmlspecialchars($date1, ENT_QUOTES);
+  $full_text = htmlspecialchars($full_text, ENT_QUOTES);
+  $date_nextd = htmlspecialchars($date_nextd, ENT_QUOTES);
+  $html_in = htmlspecialchars($html_in, ENT_QUOTES);
+
 /*запись данных в базу*/
+$link_test=mysqli_query($bd, "SELECT * FROM `php_dns`.`habra_post`");
+if(!$link_test){
+  $bd-> query("INSERT IGNORE INTO habra_post SET title='$title1', text_body='$text1', reg_date='$date1', full_text='$full_text', next_date='$date_nextd', link='$html_in'");
+      if($bd==false)
+      {
+          // echo "<br>Oll OK";
+      // }
+      // else
+      // {
+         echo mysqli_error();
+      }
+}}}
 
-// $title1='Заголовок1';
-// $text1='текст1';
-// $date1='2016-05-28';
-// $full_text='весь текст бла бла бла';
-// $date_nextd='2016-05-29';
-// $html_in='http://habrahabr.ru';
+//Вывод инфы из MySQL
 
+$sql_out = mysqli_query($bd,
+"SELECT
+  `id`,
+  `title`,
+  `text_body`,
+  `reg_date`,
+  `full_text`,
+  `next_date`,
+  `link`
+FROM
+  `php_dns`.`habra_post`");
 
-// $bd-> query("INSERT INTO habra_post SET title='$title1'");
-$bd-> query("INSERT INTO habra_post SET title='$title1', text_body='$text1', reg_date='$date1', full_text='$full_text', next_date='$date_nextd', link='$url_b[3]'");
-    if($bd==true)
-    {
-        echo "<br>Oll OK";
-    }
-    else
-    {
-       echo mysqli_error();
-    }
+$result=null;
+
+while ($result = mysqli_fetch_assoc($sql_out)) {
+    echo htmlspecialchars_decode($result['title']);
+    echo htmlspecialchars_decode($result['text_body']);
+    echo htmlspecialchars_decode($result['reg_date']);
+    echo htmlspecialchars_decode($result['full_text']);
+    echo htmlspecialchars_decode($result['next_date']);
+    echo htmlspecialchars_decode($result['link']);
 }
+
+// echo htmlspecialchars_decode($date_nextd);
+// echo htmlspecialchars_decode($date1);
+// echo htmlspecialchars_decode($title1);
+// echo htmlspecialchars_decode($text1);
+// echo htmlspecialchars_decode($html_in);
+// echo htmlspecialchars_decode($full_text);
 ?>
